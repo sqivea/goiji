61802634e4de4f8451918b813fb4d1ac1de74233
Cirill Usachov <usachov.ciri@proton.me>
2024-02-06 02:55:10 +0200

    chore: :construction: create a skeleton with grid and tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,35 @@
+import { useSelector, useDispatch } from 'react-redux'
+
+import Grid from 'components/Grid'
+import Tab from 'components/Tab'
+import { RootState } from 'services/store'
+import { TabNames, switchToTab } from 'services/store/slices/tabs'
+
 function App() {
+  const selectedTab = useSelector((state: RootState) => state.tabs.selectedTab)
+  const dispatch = useDispatch()
+
   return (
-    <div>
-      <p>Hello World</p>
-    </div>
+    <Grid>
+      <Tab
+        isSelected={selectedTab === TabNames.TAB_ADD_NEW}
+        callback={() => {
+          dispatch(switchToTab(TabNames.TAB_ADD_NEW))
+        }}
+      >
+        Add new
+      </Tab>
+      <Tab
+        isSelected={selectedTab === TabNames.TAB_SEARCH}
+        callback={() => {
+          dispatch(switchToTab(TabNames.TAB_SEARCH))
+        }}
+      >
+        Search
+      </Tab>
+      <div className="border" />
+      <div className="border" />
+    </Grid>
   )
 }
 
