83c6604785455160fcbc3a4061ce1a043c5e8d39
Cirill Usachov <usachov.ciri@proton.me>
2024-02-11 07:54:22 +0200

    chore: :lipstick: fix tabs width and columns

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,17 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from 'components/Tabs'
 function App() {
   return (
     <main className="flex size-full justify-center pt-[clamp(1rem,20vh,100px)]">
-      <Tabs defaultValue="add-new" className="w-[min(40rem,90vw)]">
-        <TabsList>
-          <TabsTrigger value="add-new">Add new</TabsTrigger>
+      <Tabs defaultValue="add-phrase" className="w-[min(40rem,90vw)]">
+        <TabsList className="grid w-full grid-cols-3">
+          <TabsTrigger value="add-phrase">Add new phrase</TabsTrigger>
+          <TabsTrigger value="add-word">Add new word</TabsTrigger>
           <TabsTrigger value="search">Search</TabsTrigger>
         </TabsList>
-        <TabsContent value="add-new">
+        <TabsContent value="add-phrase">
           <CardAddNew />
         </TabsContent>
-        <TabsContent value="search">Search</TabsContent>
+        <TabsContent value="add-word" />
+        <TabsContent value="search" />
       </Tabs>
     </main>
   )
